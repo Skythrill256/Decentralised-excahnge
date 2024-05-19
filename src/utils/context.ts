f43e64a3366d6cb794cb41f8b3c594f848457b6a
Skythrill256 <anjishnuganguly773@gmail.com>
2024-05-19 16:56:44 +0530

    first commit

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -1,10 +1,11 @@
 import { BigNumber,ethers } from "ethers"
 import { contract,tokenContract } from "./contract"
 import {toEth, toWei} from './utils'
+import { error } from "console"
 
 const contractAddress: string = "fun"
 
-export const swapToEth = async (tokenName: string , amount: string): Promise<void> =>{
+export const swapEthToToken = async (tokenName: string , amount: string): Promise<void> =>{
   try{
     let tx = {value: toWei(amount)}
     const contractObj = await contract()
@@ -15,6 +16,7 @@ export const swapToEth = async (tokenName: string , amount: string): Promise<voi
     
     const data = await contractObj.swapToEth(tokenName,tx)
     const reciept = await data.wait()
+    return reciept
   }
   catch(e){
     console.log(e)
@@ -41,4 +43,115 @@ export const hasValidAllowance = async (owner: string , tokenName: string ,amoun
   } catch (e) {
     console.log(e)
   }
+}
+
+export const swapTokenToEth = async (tokenName: string , amount: string): Promise<void> =>{
+  try {
+    const contractObj = await contract()
+    if (!contractObj){
+      console.error("Contract object is undefined")
+      return
+    }
+    const data = await contractObj.swapTokenToEth(tokenName, toWei(amount))
+    const reciept = await data.wait()
+    return reciept
+  }
+  catch(e){
+    console.log(e)
+  }
+}
+
+export const swapTokenToToken= async (srcToken:string , destToken:string , amount:string): Promise<void> =>{
+  try{
+    const contractObj = await contract()
+    if(!contractObj){
+      console.error(error)
+      return
+    }
+    const data = await contractObj.swapTokenToToken(srcToken,destToken,amount)
+    const reciept = await data.wait()
+    return reciept
+  }catch(e){
+    console.log(e)
+  }
+}
+
+export const getTokenBalance = async (tokenName:string , address:string): Promise<void> =>{
+  const contractObj = await contract()
+  if(!contractObj){
+    console.error(error)
+    return
+  }
+  const balance = await contractObj.getBalance(tokenName,address)
+  return balance
+}
+
+export const getTokenAddress = async (tokenName:string) =>{
+  try{
+    const contractObj = await contract()
+    if(!contractObj){
+      console.error(error)
+      return
+    }
+    const tokenAddress = await contractObj.getTokenAddress(tokenName)
+    return tokenAddress
+  }
+catch(e){
+  console.log(error)
+  }
+}
+
+export const increaseAllowance = async (tokenName:string, amount:string): Promise<void>=>{
+ try{
+   const contractObj = await contract()
+  if(!contractObj){
+    console.error(error)
+    return
+  }
+  const address = await contractObj.getTokenAddress(tokenName)
+  const tokenContractObj = await tokenContract(address)
+  if(!tokenContractObj){
+    console.error(error)
+    return
+  }
+  const data = await tokenContract.approve(contractAddress,toWei(amount))
+  const receipt = await data.wait()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+  return receipt                                                                                                                                                                                           
+ }catch(e){
+   console.log(error)
+ }
+}
+
+export const getAllHistory = async (): Promise<{ historyId: number, tokenA: string, tokenB: string, inputValue: string, outputValue: string, userAddress: string }[] | void> => {
+  try {
+    const contractObj = await contract();
+    if (!contractObj) {
+      console.error(error);
+      return;
+    }
+    const getAllHistory = await contractObj.getAllHistory();
+    const historyTransaction = getAllHistory.map((history: {
+      historyId: BigNumber,
+      tokenA: string,
+      tokenB: string,
+      inputValue: string,
+      outputValue: string,
+      userAddress: string
+    }): { historyId: number, tokenA: string, tokenB: string, inputValue: string, outputValue: string, userAddress: string } => ({
+      historyId: history.historyId.toNumber(),
+      tokenA: history.tokenA,
+      tokenB: history.tokenB,
+      inputValue: toEth(history.inputValue),
+      outputValue: toEth(history.outputValue),
+      userAddress: history.userAddress,
+    }));
+    return historyTransaction;
+  } catch (e) {
+    console.log(error);
+  }
+}
+
+const toWei = (amount: string): BigNumber => {
+  const toWei = ethers.utils.parseUnits(amount.toString())
+  return toWei
 }
